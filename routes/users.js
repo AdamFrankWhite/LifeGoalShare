c0672525807c46f62c24daca1bd7230e3eb36eed
Adam White <adamfrankwhite@gmail.com>
2020-03-25 17:04:52 +0000

    added signup/login routes

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,7 @@ router.route("/").get((req, res) => {
     .catch(err => res.status(400).json("Error:" + err));
 });
 
-router.route("/register").post((req, res) => {
+router.route("/signup").post((req, res) => {
   const { username, password, email } = req.body;
   const newUser = new User({
     username,
@@ -18,10 +18,30 @@ router.route("/register").post((req, res) => {
   console.log(req.body);
   bcrypt.hash(password, 10, function(err, hash) {
     newUser.password = hash;
+    if ()
     newUser
       .save()
-      .then(() => res.json("User added!"))
+      .then(() => res.json(`${username} signed up successfully!`))
       .catch(err => res.status(400).json("Error is... " + err));
+  });
+});
+
+router.route("/login").post((req, res) => {
+  const user = {
+    username: req.body.username,
+    password: req.body.password
+  };
+
+  User.authenticate(user.username, user.password, (error, user) => {
+    if (error || !user) {
+      let err = new Error();
+      err.status = 401;
+      err.message = "User or password do not match";
+      return res.json(err);
+    } else {
+      console.log("Logged In");
+      return res.json("success");
+    }
   });
 });
 
